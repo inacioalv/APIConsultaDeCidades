82424a8ce7481ed2bbca18b8928bd3908618e487
inacioalv <51245154+inacioalv@users.noreply.github.com>
2021-05-08 10:57:56 -0300

    initial

---
+++ src/main/java/com/inacioalves/APIConsultaDeCidades/ApiConsultaDeCidadesApplication.java
@@ -0,0 +1,13 @@
+package com.inacioalves.APIConsultaDeCidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsultaDeCidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsultaDeCidadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inacioalves/APIConsultaDeCidades/ApiConsultaDeCidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inacioalves.APIConsultaDeCidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsultaDeCidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
